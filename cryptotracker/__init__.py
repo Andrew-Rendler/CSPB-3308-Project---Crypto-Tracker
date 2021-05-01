3142b4759a2b26a9d92d27d79a0eb38dc4e4ad6f
Ryan Power <ryan.power@live.com>
2021-05-01 11:04:15 -0700

    Add user table to db and ability to log in

--- cryptotracker/__init__.py
+++ cryptotracker/__init__.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, Response, render_template, request, jsonify
+from flask import Flask, Response, render_template, request, jsonify, redirect, url_for, flash, send_from_directory
 from flask_restful import Api
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 #!!important
 ##UNCOMMENT TO RUN ON SERVER
 from .models import db, Bitcoin
@@ -31,11 +33,17 @@ def create_app(test_config=None):
 
     #!!important
     # UNCOMMENT TO RUN ON SERVER
+    app.config['SECRET_KEY'] = '14941313be0b30eff24fe4fb35b3b52a180aa46cacb5174b'
     app.config[
         "SQLALCHEMY_DATABASE_URI"
     ] = "postgresql://dev:password@localhost:5432/dev"
     app.config["SQLALCHEMY_TRACK+MODIFICATIONS"] = False
     db.init_app(app)
+
+    ####db = SQLAlchemy(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
     migrate = Migrate(app, db)
     api = Api(app)
     api.add_resource(HistoricalEndpoint, "/historical")
@@ -62,6 +70,30 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    bits = db.Column(db.Float)
+    ethers = db.Column(db.Float)
+    doges = db.Column(db.Float)
+
+    def __repr__(self):
+        return "<User %r %r %r %r %r>" % (
+            self.id,
+            self.username,
+            self.bits,
+            self.ethers,
+            self.doges,
+        )
+
+    db.create_all()
+
     # a simple page that says hello
     @app.route("/hello")
     def hello():
@@ -74,12 +106,81 @@ def create_app(test_config=None):
         #   - multi argument: expr_1+expr_2
         # pass an empty dict if there are no endpoint args
         # res = cryptocompare_api.api_call("ratelimit+all", {})
-        cn = CryptoNews.getData()
-        return render_template("index.html", len=len(cn), news=cn)
         # pass a dict with endpoint args
         # kwargs = {"coin": "BTC", "currency": "USD", "num_entries": "30"}
         # res = cryptocompare_api.api_call("historical+daily", kwargs)
         # return res.json()
+        cn = CryptoNews.getData()
+        if (current_user.is_authenticated):
+            return render_template("index.html", len=len(cn), news=cn, logged_in=current_user.is_authenticated, user=current_user)
+        return render_template("index.html", len=len(cn), news=cn, logged_in=current_user.is_authenticated)
+
+    @app.route('/login', methods=["GET", "POST"])
+    def login():
+        if request.method == "POST":
+            username = request.form["username"]
+            password = request.form["password"]
+            user = User.query.filter_by(username=username).first()
+            if not user:
+                flash("An account has not yet been created with that username.")
+                return redirect(url_for('login'))
+            elif not check_password_hash(user.password, password):
+                flash("Incorrect password.")
+                return redirect(url_for('login'))
+            else:
+                login_user(user)
+                return redirect(url_for('index'))
+        return render_template("login.html")
+
+    @app.route("/signup", methods=["GET", "POST"])
+    def signup():
+        if request.method == "POST":
+            if User.query.filter_by(username=request.form.get('username')).first():
+                #User already exists
+                flash("An account with this username already exists.")
+                return redirect(url_for('signup'))
+
+            hashpass = generate_password_hash(
+                request.form.get('password'),
+                method='pbkdf2:sha256',
+                salt_length=8
+            )
+            new_user = User(
+                name = request.form.get("name"),
+                username = request.form.get("username"),
+                password = hashpass,
+                bits = 0,
+                ethers = 0,
+                doges = 0,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('index'))
+        return render_template("signup.html", logged_in=current_user.is_authenticated)
+
+    @app.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for('index'))
+
+    @app.route("/portfolio", methods=["GET", "POST"])
+    @login_required
+    def portfolio():
+        if request.method == "POST":
+            current_user.bits = request.form["bits"]
+            current_user.ethers = request.form["ethers"]
+            current_user.doges = request.form["doges"]
+            db.session.commit()
+
+        #### NEED TO MULTIPLY BY ACTUAL VALUE OF COINS
+        portfolio = current_user.bits * 60000
+        portfolio += current_user.doges * .30
+        portfolio += current_user.ethers * 2000
+        portfolio = "{:,.2f}".format(portfolio)
+        return render_template("portfolio.html", user=current_user, value=portfolio)
+
 
     # testing -- rendering html and displaying BTC data from API call
     @app.route("/test")
@@ -97,10 +198,6 @@ def create_app(test_config=None):
     @app.route("/test2")
     def test2():
         return render_template("test.html", info=CryptoNews.getData())
-
-    @app.route("/calc")
-    def calc():
-        return render_template("calc.html")
 
     @app.route("/form")
     def form():
